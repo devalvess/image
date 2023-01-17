7e46899eeac68c8e3a226d013446a18c15e01a64
Edilson Santos <edilsonsantosexsp@gmail.com>
2023-01-17 18:52:34 -0300

    first commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function index()
+    {
+        return view('/index');
+    }
+
+    public function image(Request $request)
+    {
+        $request->image1->storeAs('public','file.jpg');
+        $request->image2->storeAs('public','file.jpg');
+    }
+}
